1ee3f3393dee06e2d6eb1fb418e248803965483d
siamul hassan <siam.thinkcrypt@gmail.com>
2025-07-05 22:08:28 +0600

    log all pairs

---
+++ script.js
@@ -0,0 +1,15 @@
+// log all pairs of elements in an array
+
+const elements = [1, 2, 3, 4, 5];
+
+const logAllPairs = () => {
+	elements.forEach((ele, index) => {
+		elements.forEach((ele2, index2) => {
+			if (index <= index2) {
+				console.log(`Pair: ${ele}, ${ele2}`);
+			}
+		});
+	});
+};
+
+logAllPairs(elements);
